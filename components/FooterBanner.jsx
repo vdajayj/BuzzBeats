4e95d907a7ad7eebb6cce787cc32a5d01d9918cb
Ajay Vuppu <ajayvuppu3211@gmail.com>
2024-08-26 16:08:35 +0530

    Create file structure using Next.js and use sanity to handle database

---
+++ components/FooterBanner.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const FooterBanner = () => {
+  return (
+      <div>FooterBanner</div>
+  )
+}
+export default FooterBanner
---
+++ sanitybuzzbeats/schemaTypes/product.js
@@ -0,0 +1,44 @@
+import {defineType} from "sanity";
+
+let product;
+export default product = defineType({
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields:[
+        {
+          name: 'image',
+          title: 'Image',
+          type: 'array',
+          of: [{type: 'image'}],
+          options: {
+            hotspot:true,
+          }
+        },
+        {
+          name: 'name',
+          title: 'Name',
+          type: 'string',
+        },
+        {
+          name: 'slug',
+          title: 'Slug',
+          type: 'slug',
+          options:{
+            source: 'name',
+            maxLength: 90,
+          }
+        },
+        {
+          name: 'price',
+          title: 'Price',
+          type: 'number',
+        },
+        {
+          name: 'details',
+          title: 'Details',
+          type: 'string',
+        }
+  ]
+})
+
